bf46e9166f854b1e16b777cce06b041620b0c177
Cleinn <lightboltzx@gmal.com>
2024-05-06 21:01:34 +0700

    Second Commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("Hello")
+let nameInput = document.getElementById('input-name')
+
+document.getElementById('send').addEventListener("click", function() {
+    const nama =nameInput.value
+
+    if (nama == "") {
+        document.getElementById('error-text').innerHTML = "aa"
+    }
+    else {
+        document.getElementById('error-text').innerHTML = "bb"
+    }
+})
