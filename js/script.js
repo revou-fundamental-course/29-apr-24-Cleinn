1c922303cc3276f7b6a79cad5b2a6c375368bb48
Cleinn <lightboltzx@gmal.com>
2024-05-08 15:03:22 +0700

    Fourth Commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,70 @@
+//Entering value to username and option
+
 let nameInput = document.getElementById('input-name')
 
 document.getElementById('send').addEventListener("click", function() {
     const nama =nameInput.value
 
     if (nama == "") {
-        document.getElementById('error-text').innerHTML = "aa"
+        document.getElementById('error-text').innerHTML = "Please enter your name"
+    }
+    else {
+        document.getElementById('error-text3').innerHTML = ""
     }
 })
+
+let optionInput = document.getElementById('option')
+
+document.getElementById('send').addEventListener("click", function() {
+    const option =optionInput.value
+
+    if (option == "select") {
+        document.getElementById('error-text3').innerHTML = "Please select an option"
+    }
+    else {
+        document.getElementById('error-text3').innerHTML = ""
+    }
+})
+
+//Image Slider
+
+const slides = document.querySelectorAll(".slides img")
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", InitializeSlider);
+
+function InitializeSlider(){
+    if(slides.length > 0) {
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 5000);
+    }
+     
+}
+function showSlide(index) {
+    if (index >= slides.length ) {
+        slideIndex = 0;
+    }
+    else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
+function prevSlide() {
+    slideIndex--;
+    showSlide(slideIndex)
+}
+function nextSlide() {
+    slideIndex++;
+    showSlide(slideIndex);
+}
+
+//Nav Scroll
+
+function scrollToSection(sectionId) { 
+    var section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' }); 
+} 
